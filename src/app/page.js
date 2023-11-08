1d36dadfc8f5c67b19bf81bee2951e1285a87264
Ivan Yakauleu <voodoolar333@gmail.com>
2023-11-08 20:15:35 +0100

    fitst commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,60 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [scrollTop, setScrollTop] = useState(0);
+
+  useEffect(() => {
+    function handleScroll() {
+      setScrollTop(window.scrollY);
+    }
+
+    // Добавляем слушатель события скролла к window
+    window.addEventListener('scroll', handleScroll);
+
+    // Убираем слушатель события при размонтировании компонента
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const bodyStyles = {
+    '--scrollTop': `${scrollTop}px`, // Устанавливаем значение CSS переменной
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <body style={bodyStyles}>
+      <div className="wrapper">
+        <div className="content">
+          <header className="main">
+            <div className="layers">
+              <div className="layer__header">
+                <div className="layers__caption">Welcom to Parallax</div>
+                <div className="layers__title">Fairy Forest</div>
+              </div>
+              <div
+                className="layer layers__base"
+                style={{ backgroundImage: 'url("/layer-base.png")' }}></div>
+              <div
+                className="layer layers__middle"
+                style={{ backgroundImage: 'url("/layer-middle.png")' }}></div>
+              <div
+                className="layer layers__front"
+                style={{ backgroundImage: 'url("/layer-front.png")' }}></div>
+            </div>
+          </header>
+          <article className="main-article" style={{ backgroundImage: "url('/dungeon.jpg')" }}>
+            <h2 className="main-article__header">to be continued</h2>
+            <p className="main-article__paragraph">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda fugiat maiores
+              aperiam, exercitationem fuga illum voluptate, voluptatem aut sequi eius ab voluptates
+              perspiciatis inventore, sed obcaecati aspernatur esse qui. Tenetur.
+            </p>
+            <div className="copy">IvanYakauleu</div>
+          </article>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </body>
+  );
 }
