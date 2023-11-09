237003d3888a629dd000890f0a6d94707d59463a
Ivan Yakauleu <voodoolar333@gmail.com>
2023-11-09 18:29:36 +0100

    add useref

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,54 @@
+'use client';
+
+import { useEffect, useRef } from 'react';
+
+const Header = () => {
+  const scrollRef = useRef(window.scrollY);
+
+  useEffect(() => {
+    function handleScroll() {
+      scrollRef.current.style.setProperty('--scrollTop', `${window.scrollY}px`);
+    }
+
+    // Добавляем слушатель события скролла к window
+    window.addEventListener('scroll', handleScroll);
+
+    // Убираем слушатель события при размонтировании компонентcobа
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <div ref={scrollRef}>
+      <header className="main">
+        <div className="layers">
+          <div className="layer__header">
+            <div className="layers__caption">Welcom to Parallax</div>
+            <div className="layers__title">Fairy Forest</div>
+          </div>
+          <div
+            className="layer layers__base"
+            style={{ backgroundImage: 'url("/layer-base.png")' }}></div>
+          <div
+            className="layer layers__middle"
+            style={{ backgroundImage: 'url("/layer-middle.png")' }}></div>
+          <div
+            className="layer layers__front"
+            style={{ backgroundImage: 'url("/layer-front.png")' }}></div>
+        </div>
+      </header>
+      <article className="main-article" style={{ backgroundImage: "url('/dungeon.jpg')" }}>
+        <h2 className="main-article__header">to be continued</h2>
+        <p className="main-article__paragraph">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda fugiat maiores aperiam,
+          exercitationem fuga illum voluptate, voluptatem aut sequi eius ab voluptates perspiciatis
+          inventore, sed obcaecati aspernatur esse qui. Tenetur.
+        </p>
+        <div className="copy">IvanYakauleu</div>
+      </article>
+    </div>
+  );
+};
+
+export default Header;
--- src/app/page.js
+++ src/app/page.js
@@ -1,60 +1,7 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import Header from '@/Components/Header/Header';
 
 export default function Home() {
-  const [scrollTop, setScrollTop] = useState(0);
-
-  useEffect(() => {
-    function handleScroll() {
-      setScrollTop(window.scrollY);
-    }
-
-    // Добавляем слушатель события скролла к window
-    window.addEventListener('scroll', handleScroll);
-
-    // Убираем слушатель события при размонтировании компонента
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
-  const bodyStyles = {
-    '--scrollTop': `${scrollTop}px`, // Устанавливаем значение CSS переменной
-  };
-
-  return (
-    <body style={bodyStyles}>
-      <div className="wrapper">
-        <div className="content">
-          <header className="main">
-            <div className="layers">
-              <div className="layer__header">
-                <div className="layers__caption">Welcom to Parallax</div>
-                <div className="layers__title">Fairy Forest</div>
-              </div>
-              <div
-                className="layer layers__base"
-                style={{ backgroundImage: 'url("/layer-base.png")' }}></div>
-              <div
-                className="layer layers__middle"
-                style={{ backgroundImage: 'url("/layer-middle.png")' }}></div>
-              <div
-                className="layer layers__front"
-                style={{ backgroundImage: 'url("/layer-front.png")' }}></div>
-            </div>
-          </header>
-          <article className="main-article" style={{ backgroundImage: "url('/dungeon.jpg')" }}>
-            <h2 className="main-article__header">to be continued</h2>
-            <p className="main-article__paragraph">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda fugiat maiores
-              aperiam, exercitationem fuga illum voluptate, voluptatem aut sequi eius ab voluptates
-              perspiciatis inventore, sed obcaecati aspernatur esse qui. Tenetur.
-            </p>
-            <div className="copy">IvanYakauleu</div>
-          </article>
-        </div>
-      </div>
-    </body>
-  );
+  return <Header />;
 }
